758f23bc05e8951a5b0ab7fd90df3c5069f14361
Zach <zachkozar@masterzach32.net>
2018-05-23 07:32:16 -0400

    Added item tooltips

--- src/main/kotlin/net/masterzach32/spicypineapple/item/ItemPineappleSlice.kt
+++ src/main/kotlin/net/masterzach32/spicypineapple/item/ItemPineappleSlice.kt
@@ -1,11 +1,25 @@
 package net.masterzach32.spicypineapple.item
 
 import net.masterzach32.spicypineapple.tabs.SpicyPineappleTab
+import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.item.ItemFood
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 
 class ItemPineappleSlice(hungerFilled: Int, saturation: Float) : ItemFood(hungerFilled, saturation, false) {
 
+    var getTooltip: (MutableList<String>) -> Unit = {}
+
     init {
         creativeTab = SpicyPineappleTab
+    }
+
+    fun addTooltip(func: (MutableList<String>) -> Unit): ItemPineappleSlice {
+        getTooltip = func
+        return this
+    }
+
+    override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<String>, flagIn: ITooltipFlag) {
+        getTooltip(tooltip)
     }
 }
--- src/main/kotlin/net/masterzach32/spicypineapple/registry/ModItems.kt
+++ src/main/kotlin/net/masterzach32/spicypineapple/registry/ModItems.kt
@@ -7,17 +7,27 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import net.minecraft.potion.Potion
+import net.minecraft.potion.PotionEffect
 import net.minecraftforge.client.event.ModelRegistryEvent
 import net.minecraftforge.client.model.ModelLoader
 
 @Mod.EventBusSubscriber(modid = SpicyPineappleMod.MOD_ID)
 object ModItems {
 
-    val pineappleSlice = ItemPineappleSlice(2, 0.25F).setUnlocalizedName("pineapple_slice").setRegistryName("pineapple_slice")
-    val spicyPineappleSlice = ItemPineappleSlice(3, .5F).setUnlocalizedName("pineapple_slice_spicy").setRegistryName("pineapple_slice_spicy")
+    val pineappleSlice = ItemPineappleSlice(2, 0.25F).addTooltip {
+        it.add("§e+2 hearts over 5 seconds.")
+    }.setPotionEffect(PotionEffect(Potion.getPotionFromResourceLocation("regeneration")!!, 30*5, 0), 1F).setUnlocalizedName("pineapple_slice").setRegistryName("pineapple_slice")
+    val spicyPineappleSlice = ItemPineappleSlice(3, .5F).addTooltip {
+        it.add("§e+5 hearts over 5 seconds")
+    }.setPotionEffect(PotionEffect(Potion.getPotionFromResourceLocation("regeneration")!!, 30*5, 1), 1F).setUnlocalizedName("pineapple_slice_spicy").setRegistryName("pineapple_slice_spicy")
     val grilledPineappleSlice = ItemPineappleSlice(4, 1F).setUnlocalizedName("pineapple_slice_grilled").setRegistryName("pineapple_slice_grilled")
-    val goldenPineappleSlice = ItemPineappleSlice(6, 1F).setUnlocalizedName("pineapple_slice_golden").setRegistryName("pineapple_slice_golden")
-    val godlyPineappleSlice = ItemPineappleSlice(10, 2F).setUnlocalizedName("pineapple_slice_godly").setRegistryName("pineapple_slice_godly")
+    val goldenPineappleSlice = ItemPineappleSlice(6, 1F).addTooltip {
+        it.add("§e+5 hearts over 5 seconds")
+    }.setPotionEffect(PotionEffect(Potion.getPotionFromResourceLocation("regeneration")!!, 30*5, 1), 1F).setUnlocalizedName("pineapple_slice_golden").setRegistryName("pineapple_slice_golden")
+    val godlyPineappleSlice = ItemPineappleSlice(10, 2F).addTooltip {
+        it.add("§e+20 hearts over 5 seconds")
+    }.setPotionEffect(PotionEffect(Potion.getPotionFromResourceLocation("regeneration")!!, 30*5, 10), 1F).setUnlocalizedName("pineapple_slice_godly").setRegistryName("pineapple_slice_godly")
 
     fun init() {
 
